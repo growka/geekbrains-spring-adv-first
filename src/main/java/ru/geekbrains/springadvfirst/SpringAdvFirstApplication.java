68fc9086a2a65209a05ced29c0d317bb45f9bb21
growka <thegusev@gmail.com>
2021-08-13 14:59:54 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/springadvfirst/SpringAdvFirstApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.springadvfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/springadvfirst/SpringAdvFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.springadvfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
